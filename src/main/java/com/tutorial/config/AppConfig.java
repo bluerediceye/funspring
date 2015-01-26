3f4c0fba709f83113a73fb12672db3bacba9f467
Ming Li <li.ming116@gmail.com>
2015-01-26 23:42:11 +0000

    Setup spring security

---
+++ src/main/java/com/tutorial/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.tutorial.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by mli on 26/01/15.
+ */
+@Configuration
+public class AppConfig {
+    
+}
